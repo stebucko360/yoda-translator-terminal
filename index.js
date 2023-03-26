bae06bc21960f86c4148c84cb6d69449005f10f2
stephen <stebucko360@gmail.com>
2023-03-26 23:47:01 +0100

    added functionality to ask again

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import inquirer from "inquirer";
 import { yodaSpeak } from "./translator.js";
-import questions from "./inquirerQuestions.js";
+import { question1, question2 } from "./inquirerQuestions.js";
 
 const yodaTranslator = async () => {
-  const prompter = await inquirer.prompt(questions);
+  const prompter = await inquirer.prompt(question1);
   const translation = yodaSpeak(prompter.questionForYoda);
   console.log(translation);
+  const reDo = await inquirer.prompt(question2);
+
+  if (reDo.askAgain === true) {
+    yodaTranslator();
+  } else return;
 };
 
 yodaTranslator();
--- inquirerQuestions.js
+++ inquirerQuestions.js
@@ -1,7 +1,15 @@
-export default [
+export const question1 = [
   {
     type: "input",
     name: "questionForYoda",
     message: "Type your sentence and have it Yoda-fied:",
+  },
+];
+
+export const question2 = [
+  {
+    type: "confirm",
+    name: "askAgain",
+    message: "Would you like to try another?",
   },
 ];
